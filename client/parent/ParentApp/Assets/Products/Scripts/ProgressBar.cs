7619b3b497104855f4698a00c11e503992d7f586
Takuya Ueda <uedatakuya@gmail.com>
2016-03-13 10:36:06 +0900

    プログレスバー

--- client/parent/ParentApp/Assets/Products/Scripts/ProgressBar.cs
+++ client/parent/ParentApp/Assets/Products/Scripts/ProgressBar.cs
@@ -1,21 +1,44 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
-public class ProgressBar : MonoBehaviour {
+public class ProgressBar : MonoBehaviour
+{
 
-    const int maxWidth = 512;
-    const int height = 20;
+    public enum HighlightType
+    {
+        
+    }
+
+    public class Highlight
+    {
+        public HighlightType Type { get; private set; }
+
+        public float Location { get; private set; }
+
+        public Highlight(HighlightType type, float location)
+        {
+            Type = type;
+            Location = location;
+        }
+    }
 
     [SerializeField]
     Image foreground;
 
     [SerializeField]
     public float progress;
+
+    private List<Highlight> highlights;
+
+    public ProgressBar()
+    {
+        highlights = new List<Highlight>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         progress = Mathf.Min(Mathf.Max(progress, 0), 1);
         foreground.rectTransform.localScale = new Vector3(progress, 1);
-	}
+    }
 }
